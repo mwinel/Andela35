059eed41168f4fe6bd4a67e828a3c45a1da92dc0
mwinel <mwinel@live.com>
2018-11-24 12:38:56 +0300

    Add heroku db credentials

--- db.py
+++ db.py
@@ -16,7 +16,7 @@ class DatabaseConnection:
 
         try:
             self.connection = psycopg2.connect(
-                dbname=self.db_name, user='postgres', host='localhost', password='kengo1234', port=5432
+                dbname=self.d76mr1ca3nficf, user='nhjhetdsjoidkk', host='ec2-50-19-249-121.compute-1.amazonaws.com', password='7484d15b3b4f770c5d847f06c981d8d77e7abdbd092e795882600e1e11ef7479', port=5432
             )
             self.connection.autocommit = True
             self.cur = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
